e38bc2ad0462a6f7750e97ca2282612909babdd6
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2022-10-29 04:54:32 +0600

    review added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import BirthReminder from './Projects/BirthdayReminder/BirthReminder';
 import TourDisplay from './Projects/TourList/TourDisplay';
 import TodoHome from './Projects/Todo/TodoHome';
+import ReviewSlider from './Projects/ReviewSlider/ReviewSlider';
 
 function App() {
   return (
     <div>
       <TodoHome />
+      <ReviewSlider />
       <TourDisplay />
       <BirthReminder />
     </div>
